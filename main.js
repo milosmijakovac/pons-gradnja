3a07e6ccb4d5db8d1a5b8b4236cf399e4fa88a86
Milos <skapha@gmail.com>
2019-11-22 16:21:26 +0100

    final

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
+$(document).ready(function() {
+  $('[data-fancybox="gallery-agena"]').fancybox({
+    thumbs: {
+      autoStart: true,
+      axis: "x"
+    }
+  });
+  $('[data-fancybox="gallery-cocacola"]').fancybox({
+    thumbs: {
+      autoStart: true,
+      axis: "x"
+    }
+  });
+  $('[data-fancybox="gallery-beska"]').fancybox({
+    thumbs: {
+      autoStart: true,
+      axis: "x"
+    }
+  });
+  $('[data-fancybox="gallery-enveloprint"]').fancybox({
+    thumbs: {
+      autoStart: true,
+      axis: "x"
+    }
+  });
+  $('[data-fancybox="gallery-koste"]').fancybox({
+    thumbs: {
+      autoStart: true,
+      axis: "x"
+    }
+  });
+  $('[data-fancybox="gallery-krusedol"]').fancybox({
+    thumbs: {
+      autoStart: true,
+      axis: "x"
+    }
+  });
+  $('[data-fancybox="gallery-karlito"]').fancybox({
+    thumbs: {
+      autoStart: true,
+      axis: "x"
+    }
+  });
+});
+
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const menu = document.querySelector(".mobile-menu");
@@ -22,3 +67,50 @@ const navSlide = () => {
 };
 
 navSlide();
+
+
+
+
+  let paragraph = Array.from(document.querySelectorAll(".project-paragraph"));
+  let parag = paragraph.map(item => item.innerHTML)
+
+
+  let limitParagraph = (parag) => {
+
+    for(let i = 0; i < parag.length; i++) {
+      let newTitle = [];
+     const limit = 20;
+     if(parag[i].length > limit) {
+       parag[i].split(' ').reduce((acc, cur) => {
+         if(acc + cur.length <= limit) {
+           newTitle.push(cur)
+         }
+         return acc + cur.length;
+       }, 0);
+         `${newTitle.join(' ')} ...`
+     }
+  }
+
+  limitParagraph(parag);
+
+
+
+
+  
+  // const limitParagraph = (paragraph, limit = 40) => {
+  //   const newTitle = [];
+  //   if(paragraph.length > limit) {
+  //     paragraph.split(' ').reduce((acc, cur) => {
+  //       if(acc + cur.length <= limit) {
+  //         newTitle.push(cur)
+  //       }
+  //       return acc + cur.length;
+  //     }, 0);
+      
+  //     return `${newTitle.join(' ')} ...`
+  //   }
+  //   newTitle.push(cur);
+  //   return paragraph;
+  // }
+
+  // limitParagraph(paragraph)
